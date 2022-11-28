071dd52a9e9e0300845f525d93e3ac0e036fa2e1
Cleyton <cleytonssantos@yahoo.com.br>
2022-11-28 19:40:16 -0300

    feat: create dependency injection config class

---
+++ Api/Config/DependencyInjectionConfiguration.cs
@@ -0,0 +1,10 @@
+namespace Api.Config
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddDependencyInjectionConfiguration(this IServiceCollection services)
+        {
+            //TODO add dependency
+        }
+    }
+}

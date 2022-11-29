fc57f21bc20f96f1b3aaa66656ef83e6653c8498
Cleyton <cleytonssantos@yahoo.com.br>
2022-11-29 01:13:50 -0300

    feat: create account map

---
+++ Api/Data/Mapping/AccountMap.cs
@@ -0,0 +1,31 @@
+﻿using Api.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Mapping
+{
+    public class AccountMap : IEntityTypeConfiguration<AccountModel>
+    {
+        public void Configure(EntityTypeBuilder<AccountModel> builder)
+        {
+            builder.ToTable("Accounts");
+
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id)
+                .IsRequired();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("name")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Description)
+              .IsRequired()
+              .HasColumnName("description")
+              .HasMaxLength(250);
+
+        }
+    }
+}

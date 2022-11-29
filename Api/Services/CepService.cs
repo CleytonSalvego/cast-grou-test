09cda0a84b1bacf6ec08254ac984e79c061c24ce
Cleyton <cleytonssantos@yahoo.com.br>
2022-11-29 01:16:44 -0300

    feat: create cep service

---
+++ Api/Services/CepService.cs
@@ -0,0 +1,31 @@
+﻿using Api.Interfaces;
+using Api.ViewModel;
+using System.Text.RegularExpressions;
+
+namespace Api.Services
+{
+    public class CepService: ICepService
+    {
+        private readonly IHttpService _httpService;
+        private readonly IConfiguration _configuration;
+
+        public CepService(
+            IHttpService httpService,
+            IConfiguration configuration)
+        {
+            _httpService = httpService;
+            _configuration = configuration;
+        }
+
+        public Task<ResponseCepViewModel> Get(string cep)
+        {
+            var formatedCep = FormatCep(cep);
+            return _httpService.Get(formatedCep, _configuration.GetSection("CepSettings").GetSection("uri").Value);
+        }
+
+        private string FormatCep(string cep)
+        {
+            return Regex.Replace(cep, "[^0-9]+", "");
+        }
+    }
+}

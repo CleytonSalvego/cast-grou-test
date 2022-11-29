83c2e36f9541284a3975a972cc9cb77ccb5be11a
Cleyton <cleytonssantos@yahoo.com.br>
2022-11-29 01:12:25 -0300

    feat: add dependency injection on config file

--- Api/Config/DependencyInjectionConfiguration.cs
+++ Api/Config/DependencyInjectionConfiguration.cs
@@ -1,10 +1,18 @@
+using Api.Data;
+using Api.Interfaces;
+using Api.Repository;
+using Api.Services;
+
 namespace Api.Config
 {
     public static class DependencyInjectionConfiguration
     {
         public static void AddDependencyInjectionConfiguration(this IServiceCollection services)
         {
-            //TODO add dependency
+            services.AddDbContext<DataBaseContext>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<ICepService, CepService>();
+            services.AddScoped<IHttpService, HttpRestSharpService>();
         }
     }
 }

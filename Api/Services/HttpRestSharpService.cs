1077b0df4f71a5b519118e94571205dd1fd23beb
Cleyton <cleytonssantos@yahoo.com.br>
2022-11-30 00:41:12 -0300

    chore: remove comeents

--- Api/Services/HttpRestSharpService.cs
+++ Api/Services/HttpRestSharpService.cs
@@ -10,13 +10,6 @@ namespace Api.Services
         public async Task<ResponseCepViewModel> Get(string cep, string uri)
         {
             var url = $"{uri}/{cep}/json";
-            //var client = new RestClient(url);
-            //var request = new RestRequest(uri, Method.Get);
-            //request.AddHeader("Content-Type", "application/json");
-            //RestResponse response = await client.ExecuteAsync(request);
-            ////var result = response.Content;
-            //var result = JsonConvert.DeserializeObject<ResponseCepViewModel>(response.Content);
-
             var client = new RestClient(url);
             var request = new RestRequest(url);
             var response = await client.GetAsync(request);

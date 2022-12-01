eabcc0282cf256e0be12b2f3412cf0f5eee407a2
Cleyton <cleytonssantos@yahoo.com.br>
2022-11-30 23:29:54 -0300

    feat: create dependency inject config

---
+++ Client/Config/DependencyInjectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Client.Interfaces;
+using Client.Services;
+
+namespace Client.Config
+{
+    public static class DependencyInjectionConfiguration
+    {
+         public static void AddDependencyInjectionConfiguration(this IServiceCollection services)
+        {
+            services.AddHttpClient<IAccountService, AccountService>();
+        }
+    }
+}
